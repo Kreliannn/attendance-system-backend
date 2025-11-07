d7660c3582c1f1e5d5bdeb97f18479a825ed360b
Kreliannn <krelianquimson@gmail.com>
2025-11-07 16:00:01 +0800

    update

--- index.ts
+++ index.ts
@@ -2,20 +2,29 @@ import express,{ Request, Response } from 'express';
 import mongoose from 'mongoose';
 import routes from "./routes/route"
 import cors from "cors"
+import dotenv from 'dotenv';
 
+
+dotenv.config();
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
+const mongodb_uri = process.env.MONGODB_URI || "";
+
+
+
+app.set('trust proxy', 1);
   
 app.use(express.json());
 app.use(cors()); 
 app.use(routes)
 
-mongoose.connect("mongodb://localhost:27017/attendance_system")
+mongoose.connect(mongodb_uri)
 
 app.get('/', async (request: Request, response: Response) => {
-  response.send("working...........")
+  response.send("working server...........")
 });
+
 
 
 app.listen(port, () => {
